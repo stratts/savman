5a844de3c14abe1646262aaa39faa226e83f8b51
Stratton Sloane <stratton.s@outlook.com>
2016-09-06 23:15:21 +1000

    Moved to docopt

--- savman/cli.py
+++ savman/cli.py
@@ -1,50 +1,57 @@
-from savman import databaseman, gamefind, gameman, stopwatch, datapath
+'''A utility for backing up and restoring saved games.
+
+Usage:
+  savman list [--backups]
+  savman scan [--nocache]
+  savman update
+  savman load <directory>
+  savman backup <directory> [<game>] [options]  
+  savman restore <game> [<directory>] [options]
+  savman -h | --help
+  
+Commands:
+  list              Show a list of games or backups
+  scan              Perform a scan for games
+  update            Check for a database update
+  load              Load backups from directory
+  backup            Backup all games to directory, or single game if specified
+  restore           Restore game to either save location or specified directory
+
+Options:
+  -h --help         Display this screen 
+  --scan            Perform a scan for games
+  --nocache         Scan without cache, slower but can find games the
+                    regular scan missed
+  --update          Check for database update
+
+  --trim <max>      Trim backup once it exceeds <max> versions
+  --source <num>    Game location to restore or backup from
+  --target <num>    Game location to restore to
+'''
+from savman import databaseman, gamefind, gameman, stopwatch, datapath, __version__
 from savman.vbackup import Backup
 import sys
 import os
 import logging
 import time
-import argparse
+from docopt import docopt
 from threading import Thread
 
-
 def main():
-    parser = argparse.ArgumentParser(
-        description='Find and backup save games')
-    
-    parser.add_argument('-l', '--list', dest='list', action='store_true',
-        help='''List found games''')
-    parser.add_argument('-b', '--backupall', dest='backup_dir', type=str, 
-        metavar='DIR', default=None, help='Backup saves to this directory')
-    parser.add_argument('-t', '--trim', metavar=('MIN','MAX'), nargs=2, 
-        type=int, default=[0,0], help='''Trim backup to MIN versions 
-        when number exceeds MAX''')
-    parser.add_argument('-s', '--scan', dest='scan', action='store_true',
-        help='''Perform scan for new games''')
-    parser.add_argument('-n', '--nocache', dest='nocache', action='store_true',
-        help='''Don't use cache when scanning for games. Slower but can catch 
-            games the normal scan missed''')
-    parser.add_argument('-u', '--update', dest='update', action='store_true',
-        help='''Check online for updated games database''')
-    parser.add_argument('--debug', action='store_true', help=argparse.SUPPRESS)
-    
-    #sys.argv.extend('--debug'.split())
+    print('savman', __version__)
 
-    args = parser.parse_args()
-    if args.debug:
-        fil.delta = True
+    if '--debug' in sys.argv:
+        sys.argv.remove('--debug')
+        log = logging.getLogger()
         log.setLevel(logging.DEBUG)
-    if not len(sys.argv) > 1:
-        parser.print_help()
-        sys.exit()
-    if args.trim[0] > args.trim[1]:
-            parser.error('Trim MIN must be lower than MAX')
-            sys.exit(1)
-    if args.backup_dir and not os.path.isdir(args.backup_dir):
+
+    args = docopt(__doc__, version='savman {}'.format(__version__))
+
+    if args['backup'] and args['<directory>'] and not os.path.isdir(args['<directory>']):
         try:
-            os.mkdir(args.backup_dir)
+            os.mkdir(args['<directory>'])
         except FileNotFoundError:
-            path = os.path.normpath(args.backup_dir)
+            path = os.path.normpath(args['<directory>'])
             parser.error("Could not create '{}' as directory '{}' does not exist".format(
                 path, os.path.dirname(path)
             ))
@@ -58,23 +65,23 @@ def main():
         shutil.copy(os.path.join(sys._MEIPASS, 'gamedata'), dbname)
     dbman.load(dbname)
 
-    if args.update:
+    if args['update'] or args['--update']:
         if dbman.check_update(): dbman.download(dbname)  
 
     gman = gameman.GameMan(dbman.db)
     gman.cachefile = datapath('cache')
     gman.customfile = datapath('custom.txt')
     gman.load_custom()
-    if not args.nocache: gman.load_cache()
-    elif not args.scan: 
-        logging.error('--nocache specified without --scan')
+    if not args['--nocache']: gman.load_cache()
+    elif not args['scan'] and not args['--scan']: 
+        logging.error('--nocache specified without scan command')
         sys.exit(1)
 
-    if args.scan: gman.find_games() 
+    if args['scan'] or args['--scan']: gman.find_games() 
     
     gman.save_cache()
     
-    if args.list and gman.games:
+    if args['list'] and gman.games:
         maxname = max([len(game.name) for game in gman.games.values()])
         maxid = max([len(game.id) for game in gman.games.values()])
         print('\nName', ' '*(maxname-4), 'ID', ' '*(maxid-2), 'Locations')
@@ -100,7 +107,9 @@ def main():
                 #print('*', location.path, sizet)
         print('\n{} games in total.\n'.format(len(gman.games)))
     
-    if args.backup_dir: 
-        gman.backup_games(args.backup_dir, trim_min=args.trim[0], trim_max=args.trim[1])
+    if args['backup'] and args['<directory>']: 
+        if args['--trim']:
+            gman.backup_games(args['<directory>'], trim_min=2, trim_max=args['--trim'])
+        else: gman.backup_games(args['<directory>'])
         
     logging.info('Finished!')
