40387ebb3985a88c7dac75b787d9c6b4e0ce4e50
Stratton Sloane <stratton.s@outlook.com>
2016-09-07 18:49:53 +1000

    Add docopt

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'pyyaml', 'requests', 'pypiwin32', 'tqdm'
+    'pyyaml', 'requests', 'pypiwin32', 'tqdm', 'docopt'
 ]
 
 test_requirements = [
