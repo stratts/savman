16ba9a527c6448ba7570cce7b7430ef55561e660
Stratton Sloane <stratton.s@outlook.com>
2017-03-08 22:58:41 +1100

    Add find_games test

--- tests/gameman_test.py
+++ tests/gameman_test.py
@@ -2,6 +2,16 @@ import pytest
 from savman import gameman
 
 @pytest.fixture
+def gamedir(tmpdir):
+    groot = tmpdir.mkdir('Games')
+    gdir = groot.mkdir('MyGame')
+    f1 = gdir.join('test1.txt')
+    f2 = gdir.join('test2.txt')
+    f3 = gdir.join('test3.png')
+    f1.write('test1'); f2.write('test2'); f3.write('test3')
+    return gdir
+
+@pytest.fixture
 def dir1(tmpdir):
     return tmpdir.mkdir('dir1')
 
@@ -28,7 +38,6 @@ directory: {}
     return file
 
 
-
 def test_load_custom(customfile, dir1, dir2):
     gman = gameman.GameMan('DUMMY')
     gman.load_custom(str(customfile))
@@ -42,3 +51,22 @@ def test_load_custom(customfile, dir1, dir2):
     assert game2.locations[0].path == str(dir2)
     assert 'folder1/*' in game1.locations[0].include
     assert '*.png' in game1.locations[0].exclude  
+
+def test_find_games(tmpdir, gamedir):
+    database = {
+        'games': {'MyGame': {'name': 'My Game'}},
+        'locations': {
+            ('MyGame', 0): {
+                'type': 'profile', 
+                'profile_items': ['test1.txt', 'test2.txt'],
+                'profile_dir': 'MyGame',
+                'exclude': None, 'include': None, 'subdir': None
+                }
+            }
+        }
+    gman = gameman.GameMan(database)
+    gman.finder.searchpaths = [str(tmpdir)]
+    gman.find_games()
+
+    assert 'MyGame' in gman.games
+    assert gman.games['MyGame'].locations[0].path == gamedir
